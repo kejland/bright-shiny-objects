8f21727138f135d120ef5a87eab3f53a703ab878
Eun Jin Kim <eunjinkim@macbook-pro.myfiosgateway.com>
2022-02-28 03:57:28 -0500

    upload

--- main.js
+++ main.js
@@ -2,13 +2,65 @@
 // │ Your code here! │
 // └─────────────────┘
 
+function createUser(firstName, lastName){
+    let fullName = {
+        //key: value
+        firstName: firstName,
+        lastName: lastName
+    }
+    return fullName;
+}
+// Objects —> key (what you call) : value (what the key’s value is)
+
+function setAge(user, age){
+    user.age = age;
+    return user;
+}
 
 
 
+function incrementAge(user){
+    user.age = user.age+1;
+    return user;  
+}
 
 
+function fixCar(car){
+    car.needsMaintenance = false
+    return car;
+}
 
 
+function addGrades(student, newGrades){
+
+    for (let count = 0; count < newGrades.length; count++){
+        student.grades.push(newGrades[count]);
+    }
+    return student;
+}
+
+
+//car is not an array, it's an object
+function getDataType(object, key){
+    return typeof object[key];
+}//can substitute object and key with different words, and it would still work
+
+function addTodo(todos, newTodo){
+    todos.push(newTodo);
+    return todos;
+}
+
+
+function addSong(playlist, newSong){
+    playlist.songs.push(newSong);
+    playlist.duration += newSong.duration;
+    return playlist;
+}
+
+
+// function updateReportCard(){
+
+// }
 
 
 // ┌─────────────────────────────────────────────────────────────────────────┐
